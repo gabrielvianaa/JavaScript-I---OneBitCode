6bf741a75aea9dcf9f873061754dbff991c18ef4
Gabriel Viana <123520796+gabrielvianaa@users.noreply.github.com>
2023-04-21 17:37:15 -0300

    Primeiro exercício.
    Criação de um programa em JavaScript que permite salvar informações de um cadastro.

---
+++ recrutamento/recrutamento.js
@@ -0,0 +1,11 @@
+let nome = window.prompt("Digite seu nome")
+let sobrenome = window.prompt("Digite seu sobrenome:")
+let campo = window.prompt("Digite seu campo de estudo:")
+let ano = prompt("Digite seu ano de nascimento:")
+    
+alert(
+    "Cadastro realizado com sucesso!\n" +
+    "\n nome completo:" +" " + nome + sobrenome +
+    "\n Campo de estudo:" +" " + campo +
+    "\n Ano de nascimento:"+" " + ano 
+)
