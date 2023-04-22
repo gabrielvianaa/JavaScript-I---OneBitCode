58839d4911b008b65eb0bbdaf3b0ebdccc93699b
Gabriel Viana <123520796+gabrielvianaa@users.noreply.github.com>
2023-04-21 21:15:24 -0300

    Exercício 1
    programa em JavaScript que permite inserir dois valores numéricos, calcular e obter o resultado nas quatros operações aritméticas.

---
+++ calculadora/calculadora.js
@@ -0,0 +1,12 @@
+const entrada1 = prompt("Digite o primeiro termo")
+const entrada2 = prompt("Digite o segundo termo")
+
+ const x = parseFloat(entrada1)
+ const y = parseFloat(entrada2)
+
+alert(
+    "A soma dos termos =" + " " + (x + y) +
+    "\nA subtração dos termos =" + " " + (x - y)+  
+    "\nA multiplicação dos termos =" + " " + (x * y)+ 
+    "\nA divisão dos termos =" + " " + (x / y)
+)
